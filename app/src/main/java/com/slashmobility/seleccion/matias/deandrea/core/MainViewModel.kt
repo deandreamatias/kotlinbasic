cd08e98d24d88d33a694d1880da967d4450b1c2e
Matias de Andrea <deandreamatias@gmail.com>
2020-05-17 16:19:21 +0200

    Implement coroutine in API call

--- app/src/main/java/com/slashmobility/seleccion/matias/deandrea/core/MainViewModel.kt
+++ app/src/main/java/com/slashmobility/seleccion/matias/deandrea/core/MainViewModel.kt
@@ -1,15 +1,16 @@
 package com.slashmobility.seleccion.matias.deandrea.core
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.await
 
+// MAIN VIEW MODEL: Made actions to main fragment
 class MainViewModel : ViewModel() {
-    val tag: String = this.javaClass.toString()
 
     private val _response = MutableLiveData<String>()
     private val _reversed = MutableLiveData<String>()
@@ -19,19 +20,32 @@ class MainViewModel : ViewModel() {
     val reversed: LiveData<String>
         get() = _reversed
 
+    // Create a Coroutine scope using a job to be able to cancel when needed
+    private var viewModelJob = Job()
+
+    // the Coroutine runs using the Main (UI) dispatcher
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
     // Get origin from httpbin api, verify errors and populate MainFragment strings
     fun getOrigin() {
-        Api.retrofitService.getData().enqueue( object: Callback<Data> {
-            override fun onFailure(call: Call<Data>, t: Throwable) {
-                _response.value = "Failure: " + t.message
-                Log.d(tag, "Fail + ${t.message}" )
-            }
-
-            override fun onResponse(call: Call<Data>, response: Response<Data>) {
-                _response.value = response.body()?.origin
+        coroutineScope.launch {
+            // Get the Deferred object for our Retrofit request
+            val getData = Api.retrofitService.getData()
+            try {
+                // this will run on a thread managed by Retrofit
+                val data = getData.await()
+                _response.value = data.origin
                 _reversed.value = _response.value?.reversed()
-                Log.d(tag, "Success" )
+            } catch (e: Exception) {
+                _response.value = "Fail :("
+                _reversed.value = "Fail too :("
             }
-        })
+        }
+    }
+
+    // Cancel job when dispatch view model
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
